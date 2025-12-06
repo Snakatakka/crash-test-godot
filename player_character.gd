12fa9e600d71e6c80b10eb4537cd0995f36fa917
Snakatakka <etwork563@gmail.com>
2025-12-06 12:06:27 -0500

    cleaned up some code after horribly breaking it

--- player_character.gd
+++ player_character.gd
@@ -1,20 +1,21 @@
 extends CharacterBody3D
-
 @onready var sprite: AnimatedSprite3D = $Sprite
+
 var flip_speed : float = 15.0
 var face_right : bool = true
-var face_up : bool = false
 
-# func _ready() -> void:
 
 func _physics_process(delta):
+	# more than 2 directions
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	# 2 directions left/right
+	#var input_dir = Input.get_axis("ui_left", "ui_right")
 	
 	if input_dir.x > 0.0:
 		face_right = true
 	elif input_dir.x < 0.0:
 		face_right = false
-		
 	
 	if face_right:
 		sprite.rotation_degrees.y = move_toward(sprite.rotation_degrees.y, 0.0, flip_speed)
